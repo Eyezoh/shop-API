14726e37d1b1bf4871993368b9dbd1e3018da77d
Eyezoh <amumpaireisaac@gmail.com>
2020-09-18 12:37:22 +0300

    product and order routes

---
+++ api/routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.status(201).json({
+        message:'handling request to GET products'
+    })
+
+})
+router.post('/',(req,res,next)=>{
+    res.status(201).json({
+        message:'handling request to POST products'
+    })
+
+})
+
+router.get('/:productId',(req,res,next)=>{
+    const id = req.params.productId;
+    if(id==='special'){
+        res.status(200).json({
+            message:'special id created',
+            id:id
+        })
+            
+    }else{
+        res.status(200).json({
+            message:'you passed an id'
+        })
+    }
+
+})
+router.patch('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        message:'product update'
+    })
+
+})
+router.delete('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        message:'delete product'
+    })
+
+})
+module.exports = router
